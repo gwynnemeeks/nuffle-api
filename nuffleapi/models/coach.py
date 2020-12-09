0b58e2cc63b8518833ccd1eb0d1a042bf1522ec4
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-09 15:25:50 -0600

    edited models and created fixtures

--- nuffleapi/models/coach.py
+++ nuffleapi/models/coach.py
@@ -7,4 +7,3 @@ class Coach(models.Model):
     """model for the user as coach"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
-    leageAdmin = models.BooleanField()
