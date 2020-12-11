28af120a0308ae4155c74358e3ba8de4267e184d
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-11 13:52:03 -0600

    wip serializers

--- nuffleapi/views/eventTeams.py
+++ nuffleapi/views/eventTeams.py
@@ -1,9 +1,12 @@
 """View module for handling requests about event-teams"""
+
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from nuffleapi.models import EventTeam
+from nuffleapi.views.events import EventSerializer
+from nuffleapi.views.teams import TeamSerializer
 
 
 class EventTeams(ViewSet):
@@ -36,3 +39,21 @@ class EventTeams(ViewSet):
         serializer = EventTeamSerializer(
             EventTeams, many=True, context={'request': request})
         return Response(serializer.data)
+
+class EventTeamSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for game types
+
+    Arguments:
+        serializers
+    """
+
+    event = EventSerializer(many=True)
+    team = TeamSerializer(many=True)
+
+    class Meta:
+        model = EventTeam
+        url = serializers.HyperlinkedIdentityField(
+            view_name='eventteam',
+            lookup_field='id'
+        )
+        fields = ('id', 'event', 'team')
