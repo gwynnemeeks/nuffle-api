654510ceb46a8287dc40f160bd5ee34d41769305
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-11 13:43:53 -0600

    wip viewset

---
+++ nuffleapi/views/eventTeams.py
@@ -0,0 +1,38 @@
+"""View module for handling requests about event-teams"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from nuffleapi.models import EventTeam
+
+
+class EventTeams(ViewSet):
+    """Nuffle event-teams"""
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single event-teams
+
+        Returns:
+            Response -- JSON serialized event-teams
+        """
+        try:
+            event_teams = EventTeam.objects.get(pk=pk)
+            serializer = EventTeamSerializer(event_teams, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        """Handle GET requests to get all event-teams
+
+        Returns:
+            Response -- JSON serialized list of event-teams
+        """
+        EventTeams = EventTeam.objects.all()
+
+        # Note the addtional `many=True` argument to the
+        # serializer. It's needed when you are serializing
+        # a list of objects instead of a single object.
+        serializer = EventTeamSerializer(
+            EventTeams, many=True, context={'request': request})
+        return Response(serializer.data)
