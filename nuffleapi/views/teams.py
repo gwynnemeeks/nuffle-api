c4db37fde4594b5bf338f5ce9253d278b756454c
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-11 09:47:08 -0600

    wip team viewset

---
+++ nuffleapi/views/teams.py
@@ -0,0 +1,43 @@
+"""View module for handling requests about teams"""
+
+from django.core.exceptions import ValidationError
+from django.db.models import fields
+from django.http import HttpResponseServerError
+
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from nuffleapi.models import Team, Coach
+
+class Teams(ViewSet):
+    """Nuffle teams"""
+
+    def list (self, request):
+        """Handle GET requests to the teams resource
+        
+        Returns:
+            Response == JSON serialized list of teams
+        """
+        # Get all team records from the database
+        teams = Team.objects.all()
+
+        serializer = TeamSerializer (
+            teams, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    """Json serializer for teams
+
+    Arguments:
+        serializer type
+    """
+    class Meta:
+        model = Team
+        url = serializers.HyperlinkedIdentityField(
+            view_name='team',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'team_name', 'coach', 'team_type', 'team_rank', 'team_value', 'team_rerolls', 'fan_factor', 'league_name')
+        depth = 1
