c2f4790099f2876b54fab0ed16e26bae47187dd2
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-14 09:42:04 -0600

    fixed team serializer to include coach information

--- nuffleapi/models/__init__.py
+++ nuffleapi/models/__init__.py
@@ -2,6 +2,5 @@ from .coach import Coach
 from .eventnotes import EventNote
 from .events import Event
 from .eventteams import EventTeam
-from .leagues import League
 from .players import Player
 from .teams import Team
